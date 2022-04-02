266aa6471d533e3fe464d807cc1be1bc26a5c3a1
Arahndir27 <espressler18325@gmail.com>
2022-04-02 16:18:50 -0600

    Finish endpoints

--- back-end/server.js
+++ back-end/server.js
@@ -59,18 +59,18 @@ app.post('/api/photos', upload.single('photo'), async (req, res) => {
 /* FOOD ITEM ENDPOINTS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
 
 //get list of all foods in database
-app.get('/api/food', async(req, res) => {
+app.get('/api/food', async (req, res) => {
   try {
     let foods = await Food.find();
     res.send(foods);
-  } catch(error) {
+  } catch (error) {
     console.log(error);
     res.sendStatus(500);
   }
 });
 
 //create new food item and add to database; takes a title, description, and list of ingredients
-app.post('/api/food', async(req, res) => {
+app.post('/api/food', async (req, res) => {
   const food = new Food({
     title: req.body.title,
     description: req.body.description,
@@ -81,7 +81,7 @@ app.post('/api/food', async(req, res) => {
   try {
     await food.save();
     res.send(food);
-  } catch(error) {
+  } catch (error) {
     console.log(error);
     res.sendStatus(500);
   }
@@ -121,6 +121,64 @@ app.put('api/food/:id', async (req, res) => {
 
 
 /* PET ENDPOINTS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+//get list of all pets in database
+app.get('/api/pets', async (req, res) => {
+  try {
+    let pets = await Pet.find();
+    res.send(pets);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+//create new food item and add to database; takes a title, description, and list of ingredients
+app.post('/api/pets', async (req, res) => {
+  const pet = new Pet({
+    name: req.body.name,
+    favoriteFood: req.body.favoriteFood,
+    bio: req.body.bio,
+    path: req.body.path,
+  });
+  try {
+    await pet.save();
+    res.send(pet);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+
+//Update food item
+app.put('api/pets/:id', async (req, res) => {
+  try {
+    let pet = await Pet.findOne(req.body._id);
+    pet.name = req.body.name;
+    pet.favoriteFood = req.body.favoriteFood;
+    pet.bio = req.body.bio;
+    await pet.save();
+    res.send(pet);
+  }
+  catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+//Delete food item
+app.put('api/pets/:id', async (req, res) => {
+  try {
+    await Pet.deleteOne({
+      _id: req.params.id
+    });
+    res.sendStatus(200);
+  }
+  catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
